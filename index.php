59a1c2635b85e919bae41728d0c3f1592a998046
Kiss Kornél <kornel@kisskornel.com>
2022-07-05 15:32:18 +0200

    Update Picture Sizing

--- index.php
+++ index.php
@@ -4,34 +4,6 @@ $COMPLIANCE='This site is compliant with the Creative Commons Licenses';
 $STYLESHEET='/cc.css';
 $LIC='cc0';
 ?>
-<?php
-function lic(){
-    if ($LIC == 'cc0'){
-        echo '<center><a href="https://creativecommons.org/publicdomain/zero/1.0/"><img src="/lic/img/cc0.png" width="50%" height="50%"></img></a></center>';
-    }
-    if ($LIC == 'smrr'){
-        echo '<center><a href="https://nothing"><img src="/lic/img/ccsmr.png" width="50%" height="50%"></img></a></center>';
-    }
-    if ($LIC == 'by'){
-        echo '<center><a href="https://creativecommons.org/licenses/by/4.0/"><img src="/lic/img/by.png" width="50%" height="50%"></img></a></center>';
-    }
-    if ($LIC == 'by-sa'){
-        echo '<center><a href="https://creativecommons.org/licenses/by-sa/4.0/"><img src="/lic/img/by-sa.png" width="50%" height="50%"></img></a></center>';
-    }
-    if ($LIC == 'by-nd'){
-        echo '<center><a href="https://creativecommons.org/licenses/by-nd/4.0/"><img src="/lic/img/by-nd.png" width="50%" height="50%"></img></a></center>';
-    }
-    if ($LIC == 'by-nc'){
-        echo '<center><a href="https://creativecommons.org/licenses/by-nc/4.0/"><img src="/lic/img/by-nc.png" width="50%" height="50%"></img></a></center>';
-    }
-    if ($LIC == 'by-nc-nd'){
-        echo '<center><a href="https://creativecommons.org/licenses/by-nc-nd/4.0/"><img src="/lic/img/by-nc-nd.png" width="50%" height="50%"></img></a></center>';
-    }
-    if ($LIC == 'by-nc-sa'){
-        echo '<center><a href="https://creativecommons.org/licenses/by-nc-sa/4.0/"><img src="/lic/img/by-nc-sa.png" width="50%" height="50%"></img></a></center>';
-    }
-}
-?>
 <html>
     <head>
     <link rel="stylesheet" href="<?php echo $STYLESHEET;?>">
@@ -52,6 +24,32 @@ function lic(){
 </table>
 </center>
 <center>
-    <h3><?php lic();?></h3>
+    <h3><?php
+    if ($LIC == 'cc0'){
+        echo '<center><a href="https://creativecommons.org/publicdomain/zero/1.0/"><img src="/lic/img/cc0.png" width="8%" height="8%"></img></a></center>';
+    }
+    if ($LIC == 'smrr'){
+        echo '<center><a href="https://nothing"><img src="/lic/img/ccsmr.png" width="8%" height="8%"></img></a></center>';
+    }
+    if ($LIC == 'by'){
+        echo '<center><a href="https://creativecommons.org/licenses/by/4.0/"><img src="/lic/img/by.png" width="8%" height="8%"></img></a></center>';
+    }
+    if ($LIC == 'by-sa'){
+        echo '<center><a href="https://creativecommons.org/licenses/by-sa/4.0/"><img src="/lic/img/by-sa.png" width="8%" height="8%"></img></a></center>';
+    }
+    if ($LIC == 'by-nd'){
+        echo '<center><a href="https://creativecommons.org/licenses/by-nd/4.0/"><img src="/lic/img/by-nd.png" width="8%" height="8%"></img></a></center>';
+    }
+    if ($LIC == 'by-nc'){
+        echo '<center><a href="https://creativecommons.org/licenses/by-nc/4.0/"><img src="/lic/img/by-nc.png" width="8%" height="8%"></img></a></center>';
+    }
+    if ($LIC == 'by-nc-nd'){
+        echo '<center><a href="https://creativecommons.org/licenses/by-nc-nd/4.0/"><img src="/lic/img/by-nc-nd.png" width="8%" height="8%"></img></a></center>';
+    }
+    if ($LIC == 'by-nc-sa'){
+        echo '<center><a href="https://creativecommons.org/licenses/by-nc-sa/4.0/"><img src="/lic/img/by-nc-sa.png" width="8%" height="8%"></img></a></center>';
+    }
+    ?>
+</h3>
 </center>
 </html>
